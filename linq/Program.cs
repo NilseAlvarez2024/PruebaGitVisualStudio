902bf90cd79720792266a804cd312277f3afc4d3
NilseAlvarez2024 <167377372+NilseAlvarez2024@users.noreply.github.com>
2024-04-17 20:56:19 -0300

    Add files via upload

---
+++ linq/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sucursal> Sucursales = new List<Sucursal>();
+
+            Sucursal suc1 = new Sucursal();
+            suc1.Id = 1;
+            suc1.Name = "Melo" ;
+            suc1.Poblacion = 20000;
+
+            Sucursal suc2 = new Sucursal();
+            suc2.Id = 2;
+            suc2.Name = "Maldonado";
+            suc2.Poblacion = 150000;
+
+            Sucursal suc3 = new Sucursal();
+            suc3.Id = 3;
+            suc3.Name = "Salto";
+            suc3.Poblacion = 80000;
+
+            Sucursal suc4 = new Sucursal();
+            suc4.Id = 4;
+            suc4.Name = "San Carlos";
+            suc4.Poblacion = 40000;
+            Sucursales.Add(suc1);
+            Sucursales.Add(suc2);
+            Sucursales.Add(suc3);
+            Sucursales.Add(suc4);
+
+            Sucursales.OrderBy(x => x.Poblacion)
+                 .Where(x => x.Poblacion > 50000)
+                 .Where(x => x.Name == "Melo")
+                 .Select(x => x.Poblacion).ToList()
+                 .ForEach(x => Console.WriteLine(x));
+                
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
